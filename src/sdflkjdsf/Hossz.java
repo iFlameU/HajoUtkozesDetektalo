330afbe59998bc20a45653e21386aaa30e3d6456
Peter Hegedus <hpeter@inf.u-szeged.hu>
2019-02-20 14:30:02 +0100

    hossz elkezdve

--- src/sdflkjdsf/Hossz.java
+++ src/sdflkjdsf/Hossz.java
@@ -2,4 +2,20 @@ package sdflkjdsf;
 
 public class Hossz {
 
+	double h;
+	
+	public Hossz(double val, String me) throws WrongMeException {
+		switch(me){
+		case "cm": h = val; break;
+		case "m": h = val; break;
+		case "km": h = val; break;
+		case "in": h = val; break;
+		case "ft": h = val; break;
+		case "yd": h = val; break;
+		case "mile": h = val; break;
+		case "nm": h = val; break;
+		default: throw new WrongMeException();
+		}
+	}
+
 }
