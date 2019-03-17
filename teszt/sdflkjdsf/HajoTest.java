e5c1f3109bd1bf690ffc2318f11cf386ce0178b4
David Polynar <Polynar.david@gmail.com>
2019-03-17 20:28:54 +0100

    HajoTest [Closes: #3]
    Signed-off-by: David Polynar <Polynar.david@gmail.com>

--- teszt/sdflkjdsf/HajoTest.java
+++ teszt/sdflkjdsf/HajoTest.java
@@ -1,17 +1,32 @@
+/*
+Polynár Dávid
+FLQF7M
+*/
+
 package sdflkjdsf;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import java.security.InvalidParameterException;
+
 import static org.junit.Assert.*;
 
 public class HajoTest {
 
+    Tomeg tomeg;
+    Sebesseg sebesseg;
+    Hossz hossz;
+
     @Before
     public void setUp() throws Exception {
+        tomeg = new Tomeg(1000, "kg");
+        sebesseg = new Sebesseg(10, "km/h");
+        hossz = new Hossz(100, "m");
     }
 
     @Test
-    public void utkozik_e() {
+    public void testLetezikHajo(){
+        assertNotNull(new Hajo(tomeg, sebesseg, hossz));
     }
 }
