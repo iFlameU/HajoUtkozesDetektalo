b5e9647806a1e5ada1a50e10e6c2f02be2e76e0f
David Polynar <Polynar.david@gmail.com>
2019-03-17 20:29:34 +0100

    PozicioTest [Closes: #2]
    Signed-off-by: David Polynar <Polynar.david@gmail.com>

--- teszt/sdflkjdsf/PozicioTest.java
+++ teszt/sdflkjdsf/PozicioTest.java
@@ -1,7 +1,51 @@
+/*
+Polynár Dávid
+FLQF7M
+*/
+
 package sdflkjdsf;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.swing.text.Position;
+import java.security.InvalidParameterException;
 
 import static org.junit.Assert.*;
 
 public class PozicioTest {
 
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testUgyanArraAzInputraUgyanAztKapomX(){
+        assertEquals(new Pozicio(10,10).getX(), new Pozicio(10,10).getX());
+    }
+
+    @Test
+    public void testUgyanArraAzInputraUgyanAztKapomY(){
+        assertEquals(new Pozicio(10,10).getY(), new Pozicio(10,10).getY());
+    }
+
+    @Test
+    public void testJotKapokVissza(){
+        assertEquals(10, new Pozicio(11,10).getY());
+    }
+
+    @Test
+    public void testJotKapokVissza2(){
+        assertEquals(10, new Pozicio(10,11).getX());
+    }
+
+    @Test
+    public void testJotKapokVissza3(){
+        assertEquals(-10, new Pozicio(-10,11).getX());
+    }
+
+    @Test
+    public void testLetezikHajo(){
+        assertNotNull(new Pozicio(10, 10));
+    }
 }
